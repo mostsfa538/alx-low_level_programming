04a6b49f922a0c9abae4d3cc069bb8d8280eac01
mostsfa538 <mostafa51moktar@gmail.com>
2023-03-17 05:53:20 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        for(n = 'a'; n <= 'z'; n++)
+        {
+		if( n == 'q' || n == 'e')
+		{
+			continue;
+		}
+		else
+		{
+                putchar(n);
+		}
+        }
+        putchar ('\n') ;
+        return (0);
+}
