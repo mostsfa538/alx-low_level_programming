11332d4c9789f4acdb5b39b7e3d3798df86e36b8
mostsfa538 <mostafa51moktar@gmail.com>
2023-03-17 05:56:31 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+        int n;
+
+        srand(time(0));
+        n = rand() - RAND_MAX / 2;
+        for(n = 0; n <= 9; n++)
+        {
+                printf("%d",n);
+        }
+        printf("\n") ;
+        return (0);
+}
+
