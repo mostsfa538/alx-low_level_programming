bfe50f940b62d44c214ed9608dfbec62c70fee73
mostsfa538 <mostafa51moktar@gmail.com>
2023-03-17 11:50:01 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,16 +4,21 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-        int n;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        for( n = 0; n <= 9; n++)
-        {
-                putchar(n+'0');
-        }
-        putchar('\n');
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for(n = 0; n <= 9; n++)
+	{
+	putchar(n + '0');
+	}
+	putchar('\n');
+	return(0);
 }
