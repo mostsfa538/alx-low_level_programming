539a8e1fded54dbb6cbe4345386d71470f70772c
mostsfa538 <mostafa51moktar@gmail.com>
2023-03-17 06:28:10 -0700

    variables_if_else_while

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,10 +13,13 @@ int main(void)
         for( n = 0; n <= 9; n++)
         {
                 putchar(n+'0');
+                if(n==9)
+                {
+                    break;
+                }
                 putchar(',');
                 putchar(' ');
         }
         putchar('\n');
         return (0);
 }
-
