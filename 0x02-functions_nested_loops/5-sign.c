47817b60726914d07487c9cafa8f44a95bf928c3
mostsfa538 <mostafa51moktar@gmail.com>
2023-03-21 05:52:32 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,13 +9,23 @@
  */
 int print_sign(int n)
 {
+
 	if (n > 0)
+	{
 		_putchar('+');
-	return (1);
-	else if (n == 0)
-		_putchar('0');
+		return (1);
+	}	else if (n == 0)
+
+}
+
+	_putchar(48);
 	return (0);
-	else if (n < 0)
-		_putchar('1');
-	return (-1);
+        } else if (n < 0)
+        {
+                _putchar('-');
+
+        }
+
+                return (-1);
+
 }
