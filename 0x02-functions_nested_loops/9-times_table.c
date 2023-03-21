4ba06d2a37a81651f54eda2dea0323d0ff44be40
mostsfa538 <mostafa51moktar@gmail.com>
2023-03-21 06:26:03 -0700

    functions_nested_loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,7 +10,7 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-                for (j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
 			if (j == 0)
@@ -23,11 +23,11 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-                                _putchar(k + '0');
+				_putchar(k + '0');
 			}
 			else if (k >= 10)
 			{
-                        	_putchar(',');
+				_putchar(',');
 				_putchar(' ');
 				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
