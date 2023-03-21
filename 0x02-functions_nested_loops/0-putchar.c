758544444c725548e6d13252d109992f433d6db0
mostsfa538 <mostafa51moktar@gmail.com>
2023-03-21 05:03:50 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-
- * main - print _putchar
-
- * is written by mbah
-
- * Return: 0
-
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 
