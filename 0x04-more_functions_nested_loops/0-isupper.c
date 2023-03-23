9df36a15a2f49762beeae7e74849d055431b5fd3
mostsfa538 <mostafa51moktar@gmail.com>
2023-03-23 05:22:24 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* largest_number - returns the largest of 3 numbers
+* @a: first integer
+* @b: second integer
+* @c: third integer
+* Return: largest number
+*/
+int _isupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return 0;
+	else
+		return 1;
+}
