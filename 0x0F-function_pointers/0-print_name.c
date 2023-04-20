b67fd3b7b46670bfae8ce51e7f04e69a6bb14cc9
mostsfa538 <mostafa51moktar@gmail.com>
2023-04-20 01:11:49 -0700

    function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing.
+ */
+void print_name_as_is(char *name)
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
