5d8c5b89f3dccbeaf1eede2e55fa4d783a1c896a
mostsfa538 <mostafa51moktar@gmail.com>
2024-02-02 17:22:24 +0200

    0-hash_table_create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,30 +1,30 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_create - creates a hash table
-* @size: the size
-*
-* Return: pointer to the newly created hash table
-*/
-
+ * hash_table_create - creats a hash table
+ * @size: the size of the hash table
+ *
+ * Return: the address of the hash table
+ */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table = NULL;
+	unsigned long int i = 0;
+	hash_table_t *htable = NULL;
+	hash_node_t **arr = NULL;
 
-    hash_table = malloc(sizeof(hash_table_t));
-    if (!hash_table)
-        return NULL;
+	htable = malloc(sizeof(hash_table_t));
+	if (!htable)
+		return (NULL);
 
-    hash_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (!hash_table->array) {
-        free(hash_table);
-        return NULL;
-    }
+	arr = malloc(sizeof(hash_node_t *) * size);
+	if (!arr)
+		return (NULL);
 
-    for (unsigned long int i = 0; i < size; i++)
-        hash_table->array[i] = NULL;
+	for (; i < size; ++i)
+		arr[i] = NULL;
 
-    hash_table->size = size;
+	htable->size = size;
+	htable->array = arr;
 
-    return hash_table;
+	return (htable);
 }
