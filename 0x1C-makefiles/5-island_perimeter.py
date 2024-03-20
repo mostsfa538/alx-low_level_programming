c930f4bec321cf8641d16db9e9fe30aed99eaa06
mostsfa538 <mostafa51moktar@gmail.com>
2024-03-20 12:56:11 +0200

    Technical interview preparation

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,22 @@
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+    """
+    checkOnes = 0
+    firstShow_row = 0
+    firstShow_coulm = 0
+    lastShow_row = 0
+    lastShow_coulm = 0
+    for row in range(len(grid)):
+        for column in range(len(grid[0])):
+            if grid[row][column] == 1 and checkOnes == 0:
+                checkOnes = 1
+                firstShow_row = row
+                firstShow_coulm = column
+            elif grid[row][column] == 1:
+                lastShow_row = row
+                lastShow_coulm = column
+
+    if firstShow_row == 0:
+        return 0
+    return (lastShow_coulm - firstShow_coulm + 1) * 2 + (lastShow_row - firstShow_row + 1) * 2
