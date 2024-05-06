8783afeb2ec2e6533d61ccbe5452be5f005c96ec
mostsfa538 <mostafa51moktar@gmail.com>
2024-05-06 08:47:26 +0300

    search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,6 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t left, size_t right);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif
